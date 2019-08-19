e444274b54ffa9716fa50d416f4ed2b6fad12a82
Timofeev Maxim <slmax@inbox.ru>
2019-08-19 15:37:17 +0300

    etst2

--- src/Main.java
+++ src/Main.java
@@ -18,7 +18,7 @@ public class Main {
 
         System.out.println("Статус книги " + harryPotter.toString());
 
-        System.out.println("новая ветка " + harryPotter.toString());
+        System.out.println("н2овая ветка " + harryPotter.toString());
 
 
 
